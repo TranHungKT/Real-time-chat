c63a8eeda12c0369d20cddfd5de807d1179f2f5b
Hung Tran <Hung_Tran@epam.com>
2022-11-23 16:42:21 +0700

    setup firebase

--- app/screens/Chat/components/Header/HeaderStyles.ts
+++ app/screens/Chat/components/Header/HeaderStyles.ts
@@ -16,6 +16,7 @@ export const styles = StyleSheet.create<Styles>({
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: paddingHorizontalSpace,
+    alignItems: 'center',
   },
 
   firstColumn: {
