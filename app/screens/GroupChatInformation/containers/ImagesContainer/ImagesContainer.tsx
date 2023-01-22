ac85150962bc3672f98800143378b55fc7ec7492
Hung Tran <Hung_Tran@epam.com>
2023-01-22 15:30:56 +0700

    fixed list images of chat

--- app/screens/GroupChatInformation/containers/ImagesContainer/ImagesContainer.tsx
+++ app/screens/GroupChatInformation/containers/ImagesContainer/ImagesContainer.tsx
@@ -1,6 +1,8 @@
+import { flatMap, map } from 'lodash';
 import { useState } from 'react';
 import { FlatList, View } from 'react-native';
 import ImageView from 'react-native-image-viewing';
+import { ImageSource } from 'react-native-vector-icons/Icon';
 import { useSelector } from 'react-redux';
 
 import { fetchImagesOfGroups } from '@Services/index';
@@ -10,6 +12,9 @@ import { useQuery } from '@tanstack/react-query';
 
 import { RenderListImage } from '../../components';
 import { styles } from './ImagesContainerStyles';
+
+const KEY_FLAT_LIST = 'KEY_FLAT_LIST';
+const KEY_IMAGE_VIEW = 'KEY_IMAGE_VIEW';
 
 export const ImagesContainer = () => {
   const currentGroup = useSelector(currentGroupSelector);
@@ -27,8 +32,14 @@ export const ImagesContainer = () => {
     setIsVisible(true);
   };
 
-  const renderItem = ({ item, index }: { item: { id: string; image: string }; index: number }) => {
-    return <RenderListImage item={item} onPressImage={handlePressImage(index)} />;
+  const renderItem = ({ item, index }: { item: string; index: number }) => {
+    return (
+      <RenderListImage
+        item={item}
+        onPressImage={handlePressImage(index)}
+        key={index + item + KEY_FLAT_LIST}
+      />
+    );
   };
 
   const handleCloseImage = () => setIsVisible(false);
@@ -37,23 +48,27 @@ export const ImagesContainer = () => {
     return <></>;
   }
 
-  const imagesData = data.map((messsage) => ({ uri: messsage.image }));
+  const imagesData = data.map((messsage) => messsage.listImages.map((image) => ({ uri: image })));
+
+  const getKeyExtractorForImagesView = (item: ImageSource, index: number) =>
+    item.uri + index + KEY_IMAGE_VIEW;
 
   return (
     <>
       <View style={styles.blankView} />
       <FlatList
-        data={data}
+        data={flatMap(map(data, 'listImages'))}
         renderItem={renderItem}
         style={styles.container}
         numColumns={3}
         contentContainerStyle={styles.contentContainerStyle}
       />
       <ImageView
-        images={imagesData}
+        images={flatMap(imagesData)}
         imageIndex={currentImage}
         visible={visible}
         onRequestClose={handleCloseImage}
+        keyExtractor={getKeyExtractorForImagesView}
       />
     </>
   );
